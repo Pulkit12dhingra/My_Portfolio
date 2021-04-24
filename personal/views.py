03c1697880373b34a32cfc08484b784b10c97eb0
Pulkit12dhingra <pulkit12dhingra@gmail.com>
2021-04-24 21:01:51 +0530

    6th commit

--- personal/views.py
+++ personal/views.py
@@ -5,5 +5,5 @@ def index(request):
     return render(request, 'personal/home.html')
 
 def contact(request):
-    return render(request, 'personal/basic.html',{'content':['If you would like to contact me, please email me.','pulkit12dhingra@gmail.com']})
+    return render(request, 'personal/basic.html',{'content':['If you would like to contact me', 'Please email me at pulkit12dhingra@gmail.com ', 'My phone number is :-790559650', 'Address:- A-27 Brahmapuri Indira Nagar Lucknow']})
     
